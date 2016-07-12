5cd6215af965c6c9209a1e40207334084d832e15
Sam Lachance <slachance@gmail.com>
2016-07-11 23:38:18 -0400

    added server ip field with data persistence

--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
+// If an IP from a previous use is present, it uses that.
+if(typeof localStorage["samServerIP"] != 'undefined'){
+  document.getElementById("serverip").value = localStorage["samServerIP"];
+}
+
+var sendButton = document.getElementById("sendButton");
+
+sendButton.addEventListener("click", function(){
+  // Gets the server's IP from the input field
+  var serverIP = document.getElementById("serverip").value;
+
+  // Sets form's action to IP
+  document.getElementById('samsForm').action = serverIP;
+
+  // Stores it for later use
+  localStorage["samServerIP"] = serverIP;
+})
+
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
   var url = tabs[0].url;
   document.getElementById("url").value=url;
